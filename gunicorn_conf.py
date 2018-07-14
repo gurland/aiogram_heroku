2a404e80a8dca1226e30b80ce714a92b5f7ec384
gurland <stas.gurland@gmail.com>
2018-07-15 01:27:04 +0300

    Test

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -6,6 +6,6 @@ worker_class = 'aiohttp.GunicornWebWorker'
 
 
 def on_starting(server):
-    #loop = asyncio.get_event_loop()
-    bot.loop.run_until_complete(bot.delete_webhook())
-    bot.loop.run_until_complete(bot.set_webhook(WEBHOOK_URL))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(bot.delete_webhook())
+    loop.run_until_complete(bot.set_webhook(WEBHOOK_URL))
